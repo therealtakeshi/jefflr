d07486974d9f545d0a4ff2930e88cbb53c5336ca
Takeshi Takahashi <ttakahashi@analytics8.com>
2014-06-07 12:43:11 -0500

    Clear working dir

--- bookmarklet/toolkit.js
+++ bookmarklet/toolkit.js
@@ -11,10 +11,6 @@ var toStore = {
     colorInactive:"#C0392B"
 };
 
-
-if (!opts.colorComment) opts.colorComment = "#E0A666";
-if (!opts.colorBackground) opts.colorBackground ="#1E262C";
-
 // Puts the options in the localStorage;
 // if(!localStorage.getItem('jefflr')) {
     // Note the stringify (localStorage only accepts strings);
@@ -22,7 +18,15 @@ if (!opts.colorBackground) opts.colorBackground ="#1E262C";
 // }
 
 // Keeps it easy to access localStorage;
-var opts = JSON.parse(localStorage.getItem('jefflr'));
+var opts = function () {
+    JSON.parse(localStorage.getItem('jefflr'));
+};
+
+// Allows for colorpicker
+var checkBackground = localStorage.getItem('jefflr_opts.colorBackground');
+var checkComment = localStorage.getItem('jefflr_opts.colorComment');
+if (!checkBackground) setIt('jefflr','colorBackground',"#1E262C");
+if (!checkComment) setIt('jefflr','colorComment',"#E0A666");
 
 // Checks for beta and if found, sets beta to true;
 // (first self-invoked anon function, woo!);
@@ -129,7 +133,7 @@ function addHeart() {
     li.appendChild(element);
     li.setAttribute("id","autoheart");
     // li.style.color='#b3b3b3';
-    // li.style.background='"+opts.colorBackground+"';
+    // li.style.background='"+JSON.parse(localStorage.getItem('jefflr')).colorBackground+"';
     // li.style.width='114px';
     // li.style.height='32px';
     element.className = "toolkit_inactive";
@@ -240,13 +244,15 @@ if (beta) {
     colorpicker.setAttribute('id','background-picker');
     document.getElementsByClassName('broadcaster_details_sidebar')[0].appendChild(colorpicker);
     $('#background-picker').spectrum({
-        color: opts.colorBackground,
+        color: JSON.parse(localStorage.getItem('jefflr')).colorBackground,
         clickoutFiresChange: true,
         showButtons: false,
         chooseText: "Background",
+        preferredFormat: "hex",
         change: function () {
             var color = $('#background-picker').spectrum("get")[0].value;
-            opts.colorBackground = color;
+            setIt('jefflr','colorBackground',color);
+            console.log(color);
         }
     });
     var colorpicker2 = document.createElement('input');
@@ -254,13 +260,15 @@ if (beta) {
     colorpicker2.setAttribute('id','text-picker');
     document.getElementsByClassName('broadcaster_details_sidebar')[0].appendChild(colorpicker2);
     $('#text-picker').spectrum({
-        color: opts.colorComment,
+        color: JSON.parse(localStorage.getItem('jefflr')).colorComment,
         clickoutFiresChange: true,
         showButtons: false,
         chooseText: "Text",
+        preferredFormat: "hex",
         change: function () {
             var color = $('#text-picker').spectrum("get")[0].value;
-            opts.colorBackground = color;
+            setIt('jefflr','colorComment',color);
+            console.log(color);
         }
     });
 }
@@ -268,9 +276,9 @@ if (beta) {
 // Instantiate the normalRules var;
 var normalRules = "";
 // Main broadcaster name;
-normalRules += "h1 {color: "+opts.colorComment+"}";
+normalRules += "h1 {color: "+JSON.parse(localStorage.getItem('jefflr')).colorComment+"}";
 // Broadcaster name and broadcast title;
-normalRules += "h2 {color: "+opts.colorComment+"}";
+normalRules += "h2 {color: "+JSON.parse(localStorage.getItem('jefflr')).colorComment+"}";
 // Usernames in comment list;
 normalRules += "h3 {color: #D18026; font-weight:bold}";
 // Header in superfluous header box between comment post box and comment list;
@@ -283,7 +291,7 @@ normalRules += "h4 {color: #CCC;}";
 normalRules += "#broadcast_chat {background: url(http://www.bispoke.org/assets/gb/2483078-img_0854.jpg) 50%;background-size:148%}";
 //
 // Top Mixlr bar;
-normalRules += "header#main_header {background:"+opts.colorBackground+"}";
+normalRules += "header#main_header {background:"+JSON.parse(localStorage.getItem('jefflr')).colorBackground+"}";
 // Fixes nav index issue;
 // normalRules += "#broadcast_wrapper {z-index:10001;}";
 // Search input itself;
@@ -292,7 +300,7 @@ normalRules += ".fixed_width #main_search {display:none;}";
 normalRules += "#blockUser {-webkit-transition: all 3s ease-in-out;-moz-transition: all 3s ease-in-out;-ms-transition: all 3s ease-in-out;-o-transition: all 3s ease-in-out;transition: all 3s ease-in-out;width: 100%;padding: 7px 12px 5px 12px;font-size: 11px;border-radius: 30px;-moz-border-radius: 30px;-webkit-border-radius: 30px;-ms-border-radius: 30px;-o-border-radius: 30px;behavior: url(/PIE.htc?l1351084804);height: 24px;font-family: 'Lucida$ Sans W01 Roman','Lucida Sans','Lucida Grande','Lucida Sans Unicode',sans-serif;background: black;border: 1px solid #4d4d4d;color: #b3b3b3;margin-top:8px;margin-left:8px;}";
 //
 // Left-side column;
-normalRules += "#broadcaster_details {color: "+opts.colorComment+"; background: "+opts.colorBackground+"}";
+normalRules += "#broadcaster_details {color: "+JSON.parse(localStorage.getItem('jefflr')).colorComment+"; background: "+JSON.parse(localStorage.getItem('jefflr')).colorBackground+"}";
 // Links on left-side column (chat, crowd, stage)
 normalRules += "#broadcaster_details .user_links>li>a.active {background-color: #111920;}";
 // Links on left-side column active and hover;
@@ -300,7 +308,7 @@ normalRules += "#broadcaster_details .user_links>li>a.active:hover {background-c
 // Links on left-side column hover;
 normalRules += "#broadcaster_details .user_links>li>a:hover {background-color:#18242E}";
 // Share button on left-side column has a special rule;
-normalRules += "#broadcaster_details>#user_details>time>a.share {background:"+opts.colorBackground+"}";
+normalRules += "#broadcaster_details>#user_details>time>a.share {background:"+JSON.parse(localStorage.getItem('jefflr')).colorBackground+"}";
 // jefflr: If element is active;
 normalRules += ".toolkit_active {color:"+opts.colorActive+"}";
 // jefflr: If element is active: hover;
@@ -311,9 +319,9 @@ normalRules += ".toolkit_inactive {color:"+opts.colorInactive+"}";
 normalRules += ".toolkit_inactive:hover {}";
 //
 // Last broadcast box (two-part box above comment post box);
-normalRules += "#broadcast_chat>.comment_holder>#last_broadcaster {-webkit-box-shadow: none;box-shadow: none;border-right:1px solid #AAA;color: "+opts.colorComment+"; background: "+opts.colorBackground+"; background-color:none}";
+normalRules += "#broadcast_chat>.comment_holder>#last_broadcaster {-webkit-box-shadow: none;box-shadow: none;border-right:1px solid #AAA;color: "+JSON.parse(localStorage.getItem('jefflr')).colorComment+"; background: "+JSON.parse(localStorage.getItem('jefflr')).colorBackground+"; background-color:none}";
 // Broadcaster info (bottom two-thirds of last broadcast box);
-normalRules += "#broadcast_chat>.comment_holder>#last_broadcaster>.broadcaster_info {-webkit-box-shadow: none;box-shadow: none;border-right:1px solid #AAA;color: "+opts.colorComment+"; background: "+opts.colorBackground+"; background-color:none}";
+normalRules += "#broadcast_chat>.comment_holder>#last_broadcaster>.broadcaster_info {-webkit-box-shadow: none;box-shadow: none;border-right:1px solid #AAA;color: "+JSON.parse(localStorage.getItem('jefflr')).colorComment+"; background: "+JSON.parse(localStorage.getItem('jefflr')).colorBackground+"; background-color:none}";
 // Broadcaster info hover;
 normalRules += "#broadcast_chat>.comment_holder>#last_broadcaster>.broadcaster_info:hover {background-color: #1F2B35}";
 //
@@ -322,20 +330,20 @@ normalRules += "#broadcast_chat>.comment_holder>#last_broadcaster>.broadcaster_i
 normalRules += "@media all and (max-width:55em){#broadcast_view {margin-left:0;} #broadcast_chat>.comment_holder {left:0%;margin:0 0 70px 0px;}}";
 //
 // Stupid onload FB share header;
-normalRules += "#broadcast_chat>.comment_holder header {color: "+opts.colorComment+"; background-color: "+opts.colorBackground+";}";
+normalRules += "#broadcast_chat>.comment_holder header {color: "+JSON.parse(localStorage.getItem('jefflr')).colorComment+"; background-color: "+JSON.parse(localStorage.getItem('jefflr')).colorBackground+";}";
 // Stupid onload FB share body;
-normalRules += "#broadcast_chat>.comment_holder #facebook_one_click_share {color: "+opts.colorComment+"; background-color: "+opts.colorBackground+";-webkit-box-shadow: none;box-shadow: none;}";
+normalRules += "#broadcast_chat>.comment_holder #facebook_one_click_share {color: "+JSON.parse(localStorage.getItem('jefflr')).colorComment+"; background-color: "+JSON.parse(localStorage.getItem('jefflr')).colorBackground+";-webkit-box-shadow: none;box-shadow: none;}";
 // Stupid onload FB share body hover;
 normalRules += "#broadcast_chat>.comment_holder #facebook_one_click_share:hover {background-color: #1F2B35}";
 // Hides the nasty FB icon on the onload FB share;
 normalRules += "#broadcast_chat>.comment_holder #facebook_one_click_share>img.facebook {display:none;}";
 //
 // Comment post box (middle of comment box);
-normalRules += "#broadcast_chat>.comment_holder>#chat_box {-webkit-box-shadow: none;box-shadow: none;border-right:1px solid #AAA;color: "+opts.colorComment+"; background: "+opts.colorBackground+"; background-color:none}";
+normalRules += "#broadcast_chat>.comment_holder>#chat_box {-webkit-box-shadow: none;box-shadow: none;border-right:1px solid #AAA;color: "+JSON.parse(localStorage.getItem('jefflr')).colorComment+"; background: "+JSON.parse(localStorage.getItem('jefflr')).colorBackground+"; background-color:none}";
 // Comment post box input field (so it's not stark white);
 normalRules += "#chat_box input {background:#333}";
 // Background of heart in comment post box when it's disabled;
-normalRules += "#chat_box .action.disabled {background-color:"+opts.colorBackground+"}";
+normalRules += "#chat_box .action.disabled {background-color:"+JSON.parse(localStorage.getItem('jefflr')).colorBackground+"}";
 //
 // Superfluous "All Comments" box between comment post box and comment list;
 normalRules += "#broadcast_chat>.comment_holder header {background:none}";
@@ -343,18 +351,18 @@ normalRules += "#broadcast_chat>.comment_holder header {background:none}";
 // Comment box holder
 normalRules += "#broadcast_chat>.comment_holder {-webkit-box-shadow: none;box-shadow: none;border-right:1px solid #AAA}";
 // Comment list each comment font & background color;
-normalRules += "#broadcast_chat>.comment_holder>#comments #comment_list>li {color: "+opts.colorComment+"; background-color: "+opts.colorBackground+"}";
+normalRules += "#broadcast_chat>.comment_holder>#comments #comment_list>li {color: "+JSON.parse(localStorage.getItem('jefflr')).colorComment+"; background-color: "+JSON.parse(localStorage.getItem('jefflr')).colorBackground+"}";
 // Comment list each comment hover background color;
 normalRules += "#broadcast_chat>.comment_holder>#comments #comment_list>li:hover {background-color: #1F2B35}";
 // Comment list font size & color;
-normalRules += "#broadcast_chat>.comment_holder p {color: "+opts.colorComment+";font-size:1.1em;}";
+normalRules += "#broadcast_chat>.comment_holder p {color: "+JSON.parse(localStorage.getItem('jefflr')).colorComment+";font-size:1.1em;}";
 // Usernames in the comment list;
-normalRules += "#broadcast_chat>.comment_holder>#comments>header {-webkit-box-shadow: none;box-shadow: none;border-right:1px solid #AAA;color: "+opts.colorUsername+"; background: "+opts.colorBackground+"; background-color:none;border-top:1px solid #AAA}";
+normalRules += "#broadcast_chat>.comment_holder>#comments>header {-webkit-box-shadow: none;box-shadow: none;border-right:1px solid #AAA;color: "+opts.colorUsername+"; background: "+JSON.parse(localStorage.getItem('jefflr')).colorBackground+"; background-color:none;border-top:1px solid #AAA}";
 // Comments in comment list;
-normalRules += "#broadcast_chat>.comment_holder>#chat_box textarea {color: "+opts.colorComment+"; background:"+opts.colorBackground+";}";
+normalRules += "#broadcast_chat>.comment_holder>#chat_box textarea {color: "+JSON.parse(localStorage.getItem('jefflr')).colorComment+"; background:"+JSON.parse(localStorage.getItem('jefflr')).colorBackground+";}";
 //
 // Notification box;
-normalRules += "@media all {body.broadcast_chat .notification {-webkit-box-shadow: none;box-shadow: none;border-right:1px solid #AAA;color: "+opts.colorComment+"; background: "+opts.colorBackground+"; background-color:none; border:none; left:236px; top:47px;z-index:10009;}}";
+normalRules += "@media all {body.broadcast_chat .notification {-webkit-box-shadow: none;box-shadow: none;border-right:1px solid #AAA;color: "+JSON.parse(localStorage.getItem('jefflr')).colorComment+"; background: "+JSON.parse(localStorage.getItem('jefflr')).colorBackground+"; background-color:none; border:none; left:236px; top:47px;z-index:10009;}}";
 // Here END the rules for the normal site;
 //
 // Here BEGIN the rules for the Beta site;
@@ -362,7 +370,7 @@ betaRules = "";
 // Remove Share block;
 betaRules += "@media all and (min-width:62.5em){#broadcast_main{width:100%;}}";
 // Default behind-the-scenes (mostly);
-betaRules += "#live_page_view {background: "+opts.colorBackground+";}";
+betaRules += "#live_page_view {background: "+JSON.parse(localStorage.getItem('jefflr')).colorBackground+";}";
 // Search input itself;
 betaRules += "#main_search {display:none;}";
 // Special User block;
@@ -373,9 +381,9 @@ betaRules += "@media only screen and (min-width:62.5em) {#broadcast_masthead .br
 // Main block;
 betaRules += "#broadcast_main {background: none;}";
 // Main block (with media query);
-betaRules += "@media only screen and (min-width:62.5em) {#broadcast_main {background: "+opts.colorBackground+";}}";
+betaRules += "@media only screen and (min-width:62.5em) {#broadcast_main {background: "+JSON.parse(localStorage.getItem('jefflr')).colorBackground+";}}";
 // Header block;
-betaRules += ".hiddenStats #broadcast_masthead {color: "+opts.colorUsername+"; background: "+opts.colorBackground+";}";
+betaRules += ".hiddenStats #broadcast_masthead {color: "+opts.colorUsername+"; background: "+JSON.parse(localStorage.getItem('jefflr')).colorBackground+";}";
 // Header controls;
 betaRules += ".broadcast_main_controls {background: #202020;}";
 // Header controls (while broadcasting);
@@ -392,16 +400,16 @@ betaRules += "#follow_side_btn {background: none;}";
 // Edit Profile button (already following);
 betaRules += "#follow_side_btn.unfollow {background: none;}";
 // Left-side block artwork thing;
-betaRules += "#broadcaster_artwork .artwork_container {background-color: "+opts.colorBackground+";}";
+betaRules += "#broadcaster_artwork .artwork_container {background-color: "+JSON.parse(localStorage.getItem('jefflr')).colorBackground+";}";
 //
 // Comment block
-betaRules += "#comments_container {color: "+opts.colorUsername+"; background: "+opts.colorBackground+";}";
+betaRules += "#comments_container {color: "+opts.colorUsername+"; background: "+JSON.parse(localStorage.getItem('jefflr')).colorBackground+";}";
 // Change annoying thin white bar above comment block;
 betaRules += ".crowd_list {background:none;}";
 // Comment block header;
-betaRules += "#comments_block header {color: "+opts.colorUsername+"; background: "+opts.colorBackground+";border-bottom: 1px solid "+opts.colorUsername+";}";
+betaRules += "#comments_block header {color: "+opts.colorUsername+"; background: "+JSON.parse(localStorage.getItem('jefflr')).colorBackground+";border-bottom: 1px solid "+opts.colorUsername+";}";
 // Comments inside the block;
-betaRules += ".comment_list li {background: "+opts.colorBackground+";border-bottom: 1px dotted "+opts.colorUsername+";}";
+betaRules += ".comment_list li {background: "+JSON.parse(localStorage.getItem('jefflr')).colorBackground+";border-bottom: 1px dotted "+opts.colorUsername+";}";
 // Comments inside the block: hover;
 betaRules += ".comment_list li:hover {background: #1F2B35;}";
 // Inner box inside the comment block;
@@ -411,7 +419,7 @@ betaRules += ".comment_list .commenter_profile_img {border:1px solid "+opts.colo
 // Comment username;
 betaRules += ".comment_list .comment_info h3 {color: "+opts.colorUsername+";text-transform: uppercase;}";
 // Comment content;
-betaRules += ".comment_list .comment_info p {color: "+opts.colorComment+"; font-size:1.1em}";
+betaRules += ".comment_list .comment_info p {color: "+JSON.parse(localStorage.getItem('jefflr')).colorComment+"; font-size:1.1em}";
 // Comment history button;
 betaRules += "#comment_history {background:none;}";
 // Comment deletebutton;
@@ -425,7 +433,7 @@ betaRules += "#chat_box .action.disabled {background: none; border: none;}";
 // Input box inside post bar (large chat);
 // betaRules += "#chat_box.large_chat form textarea {background:none;border: 1px solid "+opts.colorUsername+";}";
 // Input box inside post bar (small chat);
-betaRules += "#chat_box form textarea {color: "+opts.colorComment+";background:none;border: 1px solid "+opts.colorUsername+";}";
+betaRules += "#chat_box form textarea {color: "+JSON.parse(localStorage.getItem('jefflr')).colorComment+";background:none;border: 1px solid "+opts.colorUsername+";}";
 betaRules += "@media screen {#chat_box form textarea {width:93%;}}";
 // Submit button inside post bar;
 betaRules += "#chat_box form input[type='submit'] {color: "+opts.colorUsername+";background:none;font-family: 'Avenir LT W01 85 Heavy','Helvetica Neue',Helvetica,arial,sans-serif;text-transform: uppercase;letter-spacing: 1px;text-align: center;}";
@@ -499,7 +507,6 @@ var observer = new MutationObserver(function(mutations) {
         // console.log(mutation.addedNodes[0].outerHTML);
         var cid = $(mutation.addedNodes[0].outerHTML).attr("data-cid");
         var item = $("[data-cid="+cid+"]");
-        console.log(cid);
         // console.log(item.html());
         //
         // BROKEN RIGHT NOW;
